0da9e2c344b5f9182b43d64c00f901bb607beef1
Aliaksandr Drankou <drankovalexander@gmail.com>
2018-10-20 19:05:43 +0200

    *added* basic structure, simple header and footer

---
+++ common.php
@@ -0,0 +1,53 @@
+<?php
+
+session_start();
+
+function make_header($title)
+{
+    ?>
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+        <link rel="stylesheet" href="css/styles.css">
+        <title><?php echo $title;?></title>
+    </head>
+    <body>
+    <div class="container">
+        <div class="header">
+                <div class="name">
+                    <a href="index.php">Půjčovna kostýmů</a>
+                </div>
+                <div class="logins">
+                    <a href="#">Košík</a>
+                    <a href="#">Přihlásit se</a>
+                    <a href="#">Zaregistrovat se</a>
+                </div>
+        </div>
+        <div class="nav">
+            <ul>
+                <li><a href="index.php">Úvodní strana</a></li>
+                <li><a href="#">Kostýmy</a></li>
+                <li><a href="#">Doplňky</a></li>
+                <li><a href="#">Obchodní podmínky</a></li>
+                <li><a href="#">Kontakty</a></li>
+            </ul>
+        </div>
+    <?php
+}
+
+function make_footer()
+{
+    ?>
+        <footer>
+            <div class="footer">
+                <p>IIS 2018</p>
+                <p>&copy; Aliaksandr Drankou & Roman Cabala</p>
+            </div>
+
+        </footer>
+    </div>
+    </body>
+    </html>
+    <?php
+}
--- header.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: drankou
- * Date: 14/10/18
- * Time: 11:20
- */
