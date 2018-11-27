7d61cac477ba5e72640436b1fcf64803a806c5fb
Aliaksandr Drankou <drankovalexander@gmail.com>
2018-11-27 18:13:21 +0100

    *added* reaction on registration

--- registration.php
+++ registration.php
@@ -27,16 +27,61 @@ if (isset($_POST['register_btn'])) {
      //INSERT udajov do tabulky
     $sql = "INSERT INTO KLIENT(rodne_cislo, login, heslo, jmeno, prijmeni, tel_cislo, ulice, mesto, vek) VALUES ('$personal_id', '$login', '$password', '$name', '$surname', '$tel_number', '$address', '$city', '$age')";
     if (mysqli_query($db, $sql)) {
-        echo "New record created successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($db);
+        echo '<div class="registration">
+                <h4>Jste uspesne zaregistrovan. Muzete se prihlasit</h4>
+                <form action="login.php" method="post">
+                    <label for="login">Login</label>
+                    <input type="text" name="login" id="login"><br>
+            
+                    <label for="password">Heslo</label>
+                    <input type="password" name="password" id="password"><br>
+            
+                    <input type="submit" name="login_btn" value="Přihlasit se">
+                </form>
+              </div>';
+    } else {    //TODO error message handling using error_no(same name, duplicity and so on)
+                //print out message to user and ask to try again
+        echo "Error: " . $sql . "<br>" . mysqli_errno($db);
+        echo '<div class="registration">
+                <h4>Behem registrace vznikla chyba. Zkuste znovu.</h4>  //example of message
+                <form action="registration.php" method="post">
+                    <label for=login>Login*</label>
+                    <input type="text" name="login" id="login" required><br>
+            
+                    <label for="password">Heslo*</label>
+                    <input type="password" name="password" id="password" required><br>
+            
+                    <label for="personal_id">Rodné číslo*</label>
+                    <input type="text" name="personal_id" id="personal_id" placeholder="123456/7890" required><br>
+            
+                    <label for="name">Jméno</label>
+                    <input type="text" name="name" id="name"><br>
+            
+                    <label for="surname">Příjmení</label>
+                    <input type="text" name="surname" id="surname"><br>
+            
+                    <label for="email">E-mail</label>
+                    <input type="email" name="email" id="email"><br>
+            
+            
+                    <label for="tel_number">Tel. číslo</label>
+                    <input type="tel" name="tel_number" id="tel_number"><br>
+            
+                    <label for="city">Město</label>
+                    <input type="text" name="city" id="city"><br>
+            
+                    <label for="address">Adresa</label>
+                    <input type="text" name="address" id="address"><br>
+            
+                    <label for="birth_date">Datum narození</label>
+                    <input type="date" name="birth_date" id="birth_date"><br>
+            
+                    <input type="submit" name="register_btn" value="Zaregistrovat se">
+                </form>
+              </div>';
     }
-}
-
-
-?>
-
-<div class="registration">
+} else{
+    echo '<div class="registration">
     <form action="registration.php" method="post">
         <label for=login>Login*</label>
         <input type="text" name="login" id="login" required><br>
@@ -71,9 +116,9 @@ if (isset($_POST['register_btn'])) {
 
         <input type="submit" name="register_btn" value="Zaregistrovat se">
     </form>
-</div>
-
-
+</div>';
+}
+?>
 
 <?php
 make_footer();
