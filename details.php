72f4021df42f1cca3e082cb781d09af7e205282d
Roman <fatheadsk@gmail.com>
2018-11-30 15:33:01 +0100

    *added* details, history of rental

---
+++ details.php
@@ -0,0 +1,34 @@
+<?php
+require "common.php";
+require "db_init.php";
+//session_start();
+make_header("Historie");
+?>
+<h2> Seznam výpůjček</h2>
+<?php
+
+if(isset($_SESSION['login'])) {		
+	$id = $_SESSION['id'];
+
+$sql = "SELECT * FROM vypujcka where klient = '$id'";
+$result = mysqli_query($db, $sql);
+
+if (mysqli_num_rows($result) > 0){
+	while($row = mysqli_fetch_array($result)) {
+		echo '<table class="tbl-cart"> <tr> <th> ID výpůjčky </th> <th> Datum výpůjčky </th> <th> Datum vracení </th> <th> Správce </th> <th> Suma </th> </tr>';		
+		echo "<tr><td>". $row["id_vypujcky"] ."</td><td>". $row["datum_pujceni"]. "</td><td>". $row["datum_vraceni"] . "</td><td>". $row["spravce"] . "</td><td>". $row["suma"]. "</td></tr> </table>";
+	}	
+}
+else {
+		echo '<p align ="center">Ještě jste si nic nepůjčili</p>';
+	}
+}
+else{
+	echo '<p align="center">Ještě nejste přihlašený</p>';
+}
+?>
+
+
+<?php
+make_footer();
+?>
