25b20ce31cae19133a89a7a6daef0fc23f703bbe
Roman <fatheadsk@gmail.com>
2018-11-30 18:18:00 +0100

    *added* warnings when personal id, email or login already exists

--- registration.php
+++ registration.php
@@ -24,6 +24,30 @@ if (isset($_POST['register_btn'])) {
     $years = date("Y") - date("Y", $birthdate);
     $age = $years + $adjust;
     
+
+    //select for login
+    $sql_l = "SELECT * FROM klient where login='$login'";
+    //select for email
+    $sql_e = "SELECT * FROM klient where email ='$email'";
+    //select for personal id
+    $sql_i = "SELECT * FROM klient where rodne_cislo = '$personal_id'";
+    //result for earch select
+    $res_l = mysqli_query($db,$sql_l) or die(mysqli_error($db));
+    $res_e = mysqli_query($db,$sql_e) or die(mysqli_error($db));
+    $res_i = mysqli_query($db,$sql_i) or die(mysqli_error($db));
+
+    if(mysqli_num_rows($res_l) > 0){        
+        header('location:registration.php?signup=login');     
+    } else if (mysqli_num_rows($res_e) > 0){        
+        header('location:registration.php?signup=email');        
+    } else if (mysqli_num_rows($res_i) > 0) {        
+        header('location:registration.php?signup=id');
+    } else {
+
+
+
+     //INSERT udajov do tabulky
+     $sql = "INSERT INTO KLIENT(rodne_cislo, login, heslo, jmeno, prijmeni, tel_cislo, ulice, mesto, vek, email) VALUES ('$personal_id', '$login', '$password', '$name', '$surname', '$tel_number', '$address', '$city', '$age', '$email')";
     if (mysqli_query($db, $sql)) {
         echo '<div class="registration">
                 <h4>Jste uspesne zaregistrovan. Muzete se prihlasit</h4>
@@ -37,14 +61,14 @@ if (isset($_POST['register_btn'])) {
                     <input type="submit" name="login_btn" value="Přihlasit se">
                 </form>
               </div>';
-            //INSERT udajov do tabulky
-            $sql = "INSERT INTO KLIENT(rodne_cislo, login, heslo, jmeno, prijmeni, tel_cislo, ulice, mesto, vek) VALUES ('$personal_id', '$login', '$password', '$name', '$surname', '$tel_number', '$address', '$city', '$age')";
+          
     } else {    //TODO error message handling using error_no(same name, duplicity and so on)
                 //print out message to user and ask to try again
         echo "Error: " . $sql . "<br>" . mysqli_errno($db);
         echo '<div class="registration">
                 <h4>Behem registrace vznikla chyba. Zkuste znovu.</h4>  //example of message
                 <form action="registration.php" method="post">
+
                     <label for=login>Login*</label>
                     <input type="text" name="login" id="login" required><br>
             
@@ -80,11 +104,27 @@ if (isset($_POST['register_btn'])) {
                 </form>
               </div>';
     }
-} else{
+}} else{
+    // Warnings when personal id, email or login exist   
+    $url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];    
+    if (strpos($url,'login') !== false) {         
+         echo '<div class="isa_error">
+                 <i class="fa fa-times-circle"></i>
+                     Login je již zabraný </div>';
+    }else if (strpos($url,'email') !== false) {         
+         echo '<div class="isa_error">
+                 <i class="fa fa-times-circle"></i>
+                     Email je již registrovaný </div>';
+    }else if (strpos($url,'id') !== false) {        
+        echo '<div class="isa_error">
+                 <i class="fa fa-times-circle"></i>
+                     Rodné číslo je již registrováno </div>';
+}    
+     
     echo '<div class="registration">
     <form action="registration.php" method="post">
         <label for=login>Login*</label>
-        <input type="text" name="login" id="login" required><br>
+        <input type="text" name="login" id="login" required><br>        
 
         <label for="password">Heslo*</label>
         <input type="password" name="password" id="password" required><br>
@@ -105,7 +145,7 @@ if (isset($_POST['register_btn'])) {
         <label for="tel_number">Tel. číslo</label>
         <input type="tel" name="tel_number" id="tel_number"><br>
 
-        <label for="city">Město</label>
+        <label for="city">Městoooooooooo</label>
         <input type="text" name="city" id="city"><br>
 
         <label for="address">Adresa</label>
@@ -118,6 +158,7 @@ if (isset($_POST['register_btn'])) {
     </form>
 </div>';
 }
+
 ?>
 
 <?php
