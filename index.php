c7c23481b5142e67991295a9d021479317377135
Roman <fatheadsk@gmail.com>
2018-12-03 20:15:35 +0100

    *added* homepage

--- index.php
+++ index.php
@@ -5,11 +5,12 @@ require "db_init.php";
 make_header("Uvodní strana");
 ?>
 
-<p>Obsah uvodni stranky.</p>
-
+<div class="homepage">
+<img src=images/icons/homepage.jpg>	
+<p>Vítejte v půjčovně kostýmů. V Brně sídlí již od roku 1985. Jedná se o nejstarší půjčovnu kostýmů v Brně. V naší půjčovny najdete nejnovější a trendy kostýmy. Stejně si u nás můžete půjčit také doplňky ke kostýmům jako například masku, meč a podobně. V současnosti máme i iformačný systém pro pohodlnou rezervaci z domova. Tak neváhejte a zpestřete vaši oslavu kostýmem!.</p>
+</div>	
 
 <?php
-mysqli_close($db);
 make_footer();
 ?>
 
