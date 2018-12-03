0864fd0421300ed7c69f25fc1f10e5b41556f9d0
Roman <fatheadsk@gmail.com>
2018-12-03 13:51:00 +0100

    *added* contacts but its ugly

---
+++ contacts.php
@@ -0,0 +1,34 @@
+<?php
+require "common.php";
+require "db_init.php";
+
+make_header("Kostýmy");
+?>
+<div class="add-contacts-container">
+    <div class="add-item">
+	<p ><b>Půjčovna kostýmů Brno</b><br>
+	Božetěchova 194/17<br>
+ 	612 00 Brno-Královo Pole<br>
+ 	+420 999 452 235<br>
+	honza@pujcovna.cz</p>
+
+ 	 
+ 	
+	</div>
+	<div class="add-item">
+	<p> <b> Otevírací hodiny</b>
+	<p> <b>Pondělí </b> : 8:00-18:00 <br>
+	<b> Úterý</b> : 9:00-18:00 	<br>
+	<b> Středa </b>: 9:00-18:00 <br>	
+	<b> Čtvrtek</b> : 9:00-18:00 <br>
+	<b>Pátek</b> : 9:00-18:00 	<br>
+	<b>Sobota</b> : 10:00-18:00 <br>	
+	<b>Neděle </b>: zavřeno </p>	
+	</div>	
+</div>
+
+		
+
+<?php
+make_footer();
+?>
